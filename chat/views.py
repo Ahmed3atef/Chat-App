44a9bf83a1793d4aa6bb40502ca371699c1d9a47
Ahmed@Atef <clay674302@gmail.com>
2025-12-07 01:09:41 +0200

    feat: adding logic for chat app form

--- chat/views.py
+++ chat/views.py
@@ -2,19 +2,39 @@ from django.shortcuts import get_object_or_404, redirect, render
 from django.views import View
 from django.contrib.auth.decorators import login_required
 from django.utils.decorators import method_decorator
-from chat.models import ChatGroup
+from .forms import ChatmessageCreateForm
+from .models import ChatGroup
 # from .tasks import notify_customers
 
 
-
 class Index(View):
-    @ method_decorator(login_required)
-    def get(self, request):
-        chat_group = get_object_or_404(ChatGroup, group_name="public")
-        chat_messages= chat_group.chat_messages.prefetch_related("author__profile").all()[:30]
-        return render(request, 'chat/chat.html', {"PROJECT_TITLE":"Chat APP", "chat_messages": chat_messages})
-        
+    chat_group = get_object_or_404(ChatGroup, group_name="public")
+    chat_messages = chat_group.chat_messages.prefetch_related(
+        "author__profile").all()[:30]
 
+    @method_decorator(login_required)
+    def get(self, request):
+        form = ChatmessageCreateForm()
+        return render(request, 'chat/chat.html', {
+            "PROJECT_TITLE": "Chat APP",
+            "chat_messages": self.chat_messages,
+            "form": form
+            })
+
+    @method_decorator(login_required)
+    def post(self, request):
+        form = ChatmessageCreateForm(request.POST)
+        if self.form.is_valid:
+            message = self.form.save(commit=False)
+            message.author = request.user
+            message.group = self.chat_group
+            message.save()
+            return redirect('home')
+        return render(request, 'chat/chat.html', {
+            "PROJECT_TITLE": "Chat APP",
+            "chat_messages": self.chat_messages,
+            "form": form
+            })
 
 # def send_emails(request):
 #     notify_customers.delay('Hello World!')
