a7c027929b93da457272c53528261a12070ec69d
Ahmed@Atef <clay674302@gmail.com>
2025-12-07 00:56:21 +0200

    feat: customize the layout of the chat page to get data from db

--- chat/views.py
+++ chat/views.py
@@ -1,10 +1,18 @@
-from django.shortcuts import redirect, render
+from django.shortcuts import get_object_or_404, redirect, render
 from django.views import View
+from django.contrib.auth.decorators import login_required
+from django.utils.decorators import method_decorator
+from chat.models import ChatGroup
 # from .tasks import notify_customers
 
+
+
 class Index(View):
+    @ method_decorator(login_required)
     def get(self, request):
-        return render(request, 'chat/chat.html', {"PROJECT_TITLE":"Chat APP"})
+        chat_group = get_object_or_404(ChatGroup, group_name="public")
+        chat_messages= chat_group.chat_messages.prefetch_related("author__profile").all()[:30]
+        return render(request, 'chat/chat.html', {"PROJECT_TITLE":"Chat APP", "chat_messages": chat_messages})
         
 
 
