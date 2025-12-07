fb991b41dcd9a1f10cd372dae41c941e9bc3c571
Ahmed@Atef <clay674302@gmail.com>
2025-12-07 06:30:56 +0200

    feat: adding users online tracker

--- chat/models.py
+++ chat/models.py
@@ -6,7 +6,7 @@ User = settings.AUTH_USER_MODEL
 
 class ChatGroup(models.Model):
     group_name = models.CharField(max_length=128, unique=True)
-    
+    users_online = models.ManyToManyField(User, related_name="online_in_groups", blank=True)
     def __str__(self):
         return self.group_name
     
