b950ab41946d94002b7cc3d906c33aae98ea7767
Ahmed@Atef <clay674302@gmail.com>
2026-01-01 14:21:13 +0200

    feat: Introduce common.py for comprehensive Django settings including app configuration, authentication, and REST framework, and update .dockerignore to ignore .env.

--- mainProject/settings/common.py
+++ mainProject/settings/common.py
@@ -2,16 +2,18 @@ from datetime import timedelta
 from pathlib import Path
 import os
 
+from dotenv import load_dotenv
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent.parent
-
+load_dotenv(BASE_DIR / ".env")
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
 # you can get new one from https://djecrety.ir/
-SECRET_KEY = os.environ.get('SECRET_KEY')
+SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-placeholder-for-build-purposes')
 
 # Application definition
 
